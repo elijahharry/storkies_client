95d737bbd3e4e7655862067c5fbac14c7c231ea5
elijahharry <elijah@outbounded.com>
2021-11-29 13:52:38 -0600

    Update styles.js

--- src/components/Merch/Listings/Listing/styles.js
+++ src/components/Merch/Listings/Listing/styles.js
@@ -9,7 +9,7 @@ export default makeStyles((theme) => ({
     zIndex: 1,
     "&:hover": {
       zIndex: 2,
-      filter: "drop-shadow(0 0 1000px #000)",
+      filter: "drop-shadow(0 0 80px #000)",
       [theme.breakpoints.down("sm")]: {
         filter: "drop-shadow(0 0 50px #0000001a)",
       },
